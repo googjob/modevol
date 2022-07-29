3a08e3f7702573d2eb0cb90de2b291d16bef8315
unknown <php.lk@hotmail.com>
2022-07-30 00:54:22 +0800

    new files

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const header = document.querySelector("[data-header]");
+
+navToggleBtn.addEventListener("click", function () {
+  this.classList.toggle("active");
+  header.classList.toggle("active");
+});
